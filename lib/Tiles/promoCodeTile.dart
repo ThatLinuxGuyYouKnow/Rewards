dc90647f53f9006604d46d3a68988972a005a662
Alabi Ayobami <arocket04@gmail.com>
2024-09-07 21:13:33 +0100

    added neuromorphism to PromoCodeTile

--- lib/Tiles/promoCodeTile.dart
+++ lib/Tiles/promoCodeTile.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 
 class PromoCodeTile extends StatelessWidget {
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Container(
+      height: screenHeight * 0.08,
+      width: screenWidth * 0.9,
       decoration: BoxDecoration(
-          color: Colors.white, boxShadow: [BoxShadow(color: Colors.black)]),
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(color: Colors.black.withOpacity(.3), blurRadius: 2)
+          ]),
     );
   }
 }
